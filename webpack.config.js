6bffd2eb24a378d5b1c20cb903d0f12adab0a415
Alexey <sahonchik.alexey@gmail.com>
2022-10-18 21:36:58 +0300

    require eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -19,4 +20,5 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new ESLintPlugin()],
 };
