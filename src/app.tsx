f640557c45e60c4927401ca6de6e8ca7ccc9aba0
Alexey <sahonchik.alexey@gmail.com>
2022-10-19 16:49:00 +0300

    add support of css and images

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import './styles.css'
+import LOGO from './logo.png'
+
 export const App = () => {
-    return <h1>React TypeScript Webpack Template</h1>
+    return <div id="header">
+            <img src={LOGO} alt="Todo application logo" width="135" height="110" />
+            <h1>React TypeScript Webpack Template</h1>
+        </div>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
                 },
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            }
         ],
     },
     resolve: {
