9bb0e63e77d42d16d79be2452519dc6e26b649da
Alexey <sahonchik.alexey@gmail.com>
2022-10-20 16:24:21 +0300

    configuring and using ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,33 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+  ],
   env: {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
   overrides: [
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   plugins: [
     'react'
   ],
   rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   }
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import './styles.css'
 import LOGO from './logo.png'
 
-export const App = () => {
-    return <div id="header">
+export const App = (): JSX.Element => {
+  return <div id="header">
             <img src={LOGO} alt="Todo application logo" width="135" height="110" />
             <h1>React TypeScript Webpack Template</h1>
         </div>
